914bca412eb03e3ba3e7a0ac51c41b5d60a7c5d7
403F <4o3f@proton.me>
2023-12-04 21:33:19 +0800

    feat: Add shadcn-vue

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,24 @@
-import { defineConfig } from "vite";
-import vue from "@vitejs/plugin-vue";;
+import path from "path";
+import {defineConfig} from "vite";
+import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig(async () => ({
-  plugins: [vue()],
+    plugins: [vue()],
 
-  // Vite options tailored for Tauri development and only applied in `tauri dev` or `tauri build`
-  //
-  // 1. prevent vite from obscuring rust errors
-  clearScreen: false,
-  // 2. tauri expects a fixed port, fail if that port is not available
-  server: {
-    port: 1420,
-    strictPort: true,
-  }
+    // Vite options tailored for Tauri development and only applied in `tauri dev` or `tauri build`
+    //
+    // 1. prevent vite from obscuring rust errors
+    clearScreen: false,
+    // 2. tauri expects a fixed port, fail if that port is not available
+    server: {
+        port: 1420,
+        strictPort: true,
+    },
+    /* Resolve path */
+    resolve: {
+        alias: {
+            "@": path.resolve(__dirname, "./src"),
+        },
+    },
 }));
